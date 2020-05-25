ddcbdccb59c396623ab4b577ea40bcd7a22473a8
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-05-25 08:58:56 -0300

    Retornando pacientes.

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste', (request, response) => {
-    return response.json({ message: 'White Rabb1t.' });
+const users = ['Paciente 001', 'Paciente 002', 'Paciente 003'];
+
+server.get('/users/:index', (request, response) => {
+    const { index } = request.params;
+
+    return response.json(users[index]);
 });
 
 server.listen(3000);
