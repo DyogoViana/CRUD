8528ce78e8eaebd92f07f3a4941c7ab82eb56fa0
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-05-25 09:24:13 -0300

    CRUD funcionando.

--- index.js
+++ index.js
@@ -2,12 +2,51 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json()); 
+
 const users = ['Paciente 001', 'Paciente 002', 'Paciente 003'];
+
+
+// GET
+server.get('/users', (request, response) => {
+    return response.json(users);
+});
 
 server.get('/users/:index', (request, response) => {
     const { index } = request.params;
 
     return response.json(users[index]);
+});
+
+
+// POST
+server.post('/users', (request, response) => {
+    const { name } = request.body;
+
+    users.push(name);
+
+    return response.json(users);
+});
+
+
+// PUT
+server.put('/users/:index', (request, response) => {
+    const { index } = request.params;
+    const { name } = request.body;
+
+    users[index] = name;
+
+    return response.json(users);
+});
+
+
+// DELETE
+server.delete('/users/:index', (request, response) => {
+    const { index } = request.params;
+
+    users.splice(index, 1);
+
+    return response.send();
 });
 
 server.listen(3000);
