6c7b2e722b8c452737a7c0ccb4aeb3364ce4c4f8
Dyogo Viana <dyogo.vianaa@gmail.com>
2020-05-25 16:38:28 -0300

    Middlewares.

--- index.js
+++ index.js
@@ -6,21 +6,41 @@ server.use(express.json());
 
 const users = ['Paciente 001', 'Paciente 002', 'Paciente 003'];
 
+// Middlewares
+function checarUsuarioExiste(request, response, next) {
+    if (!request.body.name) {
+        return response.status(400).json({ 
+            error: 'Nome do paciente é obrigatório.'
+        });
+    }
+    return next();
+}
+
+function checarArrayUsuario(request, response, next) {
+    const user = users[request.params.index];
+
+    if (!user) {
+        return response.status(400).json({
+            error: 'Paciente não existe.'
+        });
+    }
+    request.user = user;
+
+    return next();
+}
 
 // GET
 server.get('/users', (request, response) => {
     return response.json(users);
 });
 
-server.get('/users/:index', (request, response) => {
-    const { index } = request.params;
-
-    return response.json(users[index]);
+server.get('/users/:index', checarArrayUsuario, (request, response) => {
+    return response.json(request.user);
 });
 
 
 // POST
-server.post('/users', (request, response) => {
+server.post('/users', checarUsuarioExiste, (request, response) => {
     const { name } = request.body;
 
     users.push(name);
@@ -30,7 +50,7 @@ server.post('/users', (request, response) => {
 
 
 // PUT
-server.put('/users/:index', (request, response) => {
+server.put('/users/:index', checarArrayUsuario, checarUsuarioExiste, (request, response) => {
     const { index } = request.params;
     const { name } = request.body;
 
@@ -41,7 +61,7 @@ server.put('/users/:index', (request, response) => {
 
 
 // DELETE
-server.delete('/users/:index', (request, response) => {
+server.delete('/users/:index', checarArrayUsuario, (request, response) => {
     const { index } = request.params;
 
     users.splice(index, 1);
